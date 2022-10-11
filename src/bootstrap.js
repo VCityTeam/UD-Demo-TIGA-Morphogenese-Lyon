bb009df46eb1b0b94cd9c109fbfd6d32471f3a40
CorentinLIRIS <corentin.chignin@hotmail.fr>
2022-10-11 09:28:52 +0200

    Try some shader

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -50,53 +50,58 @@ app.start('../assets/config/config.json').then((config) => {
 
 
   //SHADER
-  const material = new udviz.THREE.RawShaderMaterial( {
 
+  const MYMAT = new udviz.THREE.ShaderMaterial({
     uniforms: {
-      time: { value: 1.0 }
+      tDiffuse: { value: null },
+      tSize: { value: new udviz.THREE.Vector2(256, 256) },
+      center: { value: new udviz.THREE.Vector2(0.5, 0.5) },
+      angle: { value: 1.57 },
+      scale: { value: 1.0 },
     },
     vertexShader: `
-      precision mediump float;
-			precision mediump int;
+    varying vec2 vUv;
 
-			uniform mat4 modelViewMatrix; // optional
-			uniform mat4 projectionMatrix; // optional
-
-			attribute vec3 position;
-			attribute vec4 color;
-
-			varying vec3 vPosition;
-			varying vec4 vColor;
-
-			void main()	{
-
-				vPosition = position;
-				vColor = color;
-
-				gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-
-			}`,
+    void main() {
+        vUv = uv;
+        gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+    }
+    `,
     fragmentShader: `
-    precision mediump float;
-			precision mediump int;
+    uniform vec2 center;
+    uniform float angle;
+    uniform float scale;
+    uniform vec2 tSize;
 
-			uniform float time;
+    uniform sampler2D tDiffuse;
 
-			varying vec3 vPosition;
-			varying vec4 vColor;
+    varying vec2 vUv;
 
-			void main()	{
+    float pattern() {
 
-				vec4 color = vec4( vColor );
-				color.r += sin( vPosition.x * 10.0 + time ) * 0.5;
+        float s = sin( angle ), c = cos( angle );
 
-				gl_FragColor = color;
+        vec2 tex = vUv * tSize - center;
+        vec2 point = vec2( c * tex.x - s * tex.y, s * tex.x + c * tex.y ) * scale;
 
-			}`,
-    side: udviz.THREE.DoubleSide,
-    transparent: true
+        return ( sin( point.x ) * sin( point.y ) ) * 4.0;
+    }
+    void main() {
+        vec4 color = texture2D( tDiffuse, vUv );
+        float average = ( color.r + color.g + color.b ) / 3.0;
+        gl_FragColor = vec4( vec3( average * 10.0 - 5.0 + pattern() ), color.a );
+    }
+    `,
+  });
 
-  } );
+  const renderTargetFX = new udviz.THREE.WebGLRenderTarget(0, 0, {
+    depthBuffer: true,
+    stencilBuffer: false,
+    format: udviz.THREE.RGBAFormat,
+    type: udviz.THREE.FloatType,
+  });
+
+
   // debugger
   //Event to select a tile set
   function onTileSelect( event ) {    
@@ -129,6 +134,27 @@ app.start('../assets/config/config.json').then((config) => {
       let arrayWithOnlyGeometryMesh = []; // Array of Vector3 with only the geometry of the cityObject picked
       let centroide = new udviz.THREE.Vector3(0, 0, 0);
 
+      const edgeDetectionComposer = new udviz.EffectComposer(app.view3D.getRenderer(), renderTargetFX);
+      const normalsPass = new udviz.RenderPass(app.view3D.getScene(), app.view3D.getCamera(), MYMAT);
+      edgeDetectionComposer.addPass(normalsPass);
+      // const sobelPass = new udviz.ShaderPass(MySobelOperatorShader);
+      // edgeDetectionComposer.addPass(sobelPass);
+      // edgeDetectionComposer.renderToScreen = false;
+
+      // cityObject3D.children[0].children[0].material[0] = MYMAT;
+      cityObject3D.children[0].children[0].frustumCulled = false;
+      cityObject3D.children[0].children[0].material[1] = MYMAT;
+      // cityObject3D.children[0].children[0].material[2] = MYMAT;
+      let quad = cityObject3D.children[0].children[0].material[1];
+      let g = app.view3D.getItownsView().mainLoop.gfxEngine;
+      quad.uniforms.tDiffuse.value = g.fullSizeRenderTarget.texture;
+      quad.uniforms.tSize.value.set(
+        g.fullSizeRenderTarget.width, g.fullSizeRenderTarget.height);
+
+      // cityObject3D.children[0].children[0].material[2].uniforms.time.value = time * 0.005;
+
+      // debugger
+
       let indexCount = cityObject.indexCount;
       let indexStart = cityObject.indexStart;
 
@@ -156,6 +182,8 @@ app.start('../assets/config/config.json').then((config) => {
       }
 
       cityObject.tile.getObject3D().content.children[meshId].geometry.attributes.position.needsUpdate = true;
+
+      // debugger;
     }
   }
 });
