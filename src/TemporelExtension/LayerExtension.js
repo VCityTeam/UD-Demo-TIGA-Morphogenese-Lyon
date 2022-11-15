db8137ef879e8ef67c936249fda8a2b30baf65fa
CorentinLIRIS <corentin.chignin@hotmail.fr>
2022-11-15 15:13:28 +0100

    Better Slider

--- src/TemporelExtension/LayerExtension.js
+++ src/TemporelExtension/LayerExtension.js
@@ -5,6 +5,7 @@
 // import { LayerManager } from 'ud-viz/src/Components/LayerManager/LayerManager';
 import * as udviz from 'ud-viz';
 import './temporalExtension.css';
+import $ from 'jquery';
 
 export class LayerExtension {
   /**
@@ -29,12 +30,12 @@ export class LayerExtension {
       <span class="incl-range"></span>
       <input name="rangeOne" value="10" min="0" max="100" step="1" type="range">
       <input name="rangeTwo" value="90" min="0" max="100" step="1" type="range">
-    </section>
+  </section>
     `;
   }
 
   windowCreated() {
-    let viewerDiv = document.getElementById('viewerDiv');
+    let viewerDiv = document.getElementById('root_View3D');
     // viewerDiv.innerHTML += this.innerContentHtml;
     let temporalDiv = document.createElement('div');
     temporalDiv.id = 'temporal-updated';
@@ -42,5 +43,36 @@ export class LayerExtension {
     
     viewerDiv.append(temporalDiv);
     // viewerDiv.innerHTML += this.innerContentHtml;
+    var rangeOne = document.querySelector('input[name="rangeOne"]'),
+      rangeTwo = document.querySelector('input[name="rangeTwo"]'),
+      outputOne = document.querySelector('.outputOne'),
+      outputTwo = document.querySelector('.outputTwo'),
+      inclRange = document.querySelector('.incl-range'),
+      updateView = function () {
+        if (this.getAttribute('name') === 'rangeOne') {
+          outputOne.innerHTML =  parseInt(this.value * 0.38) + 1980;
+          outputOne.style.left = this.value / this.getAttribute('max') * 100 + '%';
+        } else {
+          outputTwo.style.left = this.value / this.getAttribute('max') * 100 + '%';
+          outputTwo.innerHTML = parseInt(this.value * 0.38) + 1980;
+        }
+        if (parseInt(rangeOne.value) > parseInt(rangeTwo.value)) {
+          inclRange.style.width = (rangeOne.value - rangeTwo.value) / this.getAttribute('max') * 100 + '%';
+          inclRange.style.left = rangeTwo.value / this.getAttribute('max') * 100 + '%';
+        } else {
+          inclRange.style.width = (rangeTwo.value - rangeOne.value) / this.getAttribute('max') * 100 + '%';
+          inclRange.style.left = rangeOne.value / this.getAttribute('max') * 100 + '%';
+        }
+      };
+
+    document.addEventListener('DOMContentLoaded', function () {
+      updateView.call(rangeOne);
+      updateView.call(rangeTwo);
+      $('input[type="range"]').on('mouseup', function() {
+        this.blur();
+      }).on('mousedown input', function () {
+        updateView.call(this);
+      });
+    });
   }
 }
