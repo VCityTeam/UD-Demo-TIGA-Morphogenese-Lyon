c31e58779a074bdaac10085a07ac724acebf61e0
CorentinLIRIS <corentin.chignin@hotmail.fr>
2022-11-23 15:21:49 +0100

    Add dot with Berliet Data

--- src/TemporelExtension/LayerExtension.js
+++ src/TemporelExtension/LayerExtension.js
@@ -3,10 +3,9 @@
 //Components
 // import { Window } from '../node_modules/ud-viz/src/Components/GUI/js/Window';
 // import { LayerManager } from 'ud-viz/src/Components/LayerManager/LayerManager';
-import * as udviz from 'ud-viz';
 import './temporalExtension.css';
 import $ from 'jquery';
-import { View3D } from 'ud-viz/src/Views/Views';
+import * as udviz from 'ud-viz';
 
 export class LayerExtension {
   /**
@@ -20,6 +19,13 @@ export class LayerExtension {
      * the layerManager
      */
     this.layerManager = layerManager;
+
+    this.listElementDot;
+
+    this.temporalDiv;
+
+    this.windowCreated();
+    this.createdDotElementData();
   }
 
   get innerContentHtml() {
@@ -28,7 +34,6 @@ export class LayerExtension {
       <span class="output outputOne"></span>
       <span class="output outputTwo"></span>
       <span class="full-range"></span>
-      <span class="dot"></span>
       <span class="incl-range"></span>
       <input name="rangeOne" value="10" min="0" max="100" step="1" type="range">
       <input name="rangeTwo" value="90" min="0" max="100" step="1" type="range">
@@ -38,11 +43,11 @@ export class LayerExtension {
 
   windowCreated() {
     let viewerDiv = document.getElementById('root_View3D');
-    let temporalDiv = document.createElement('div');
-    temporalDiv.id = 'temporal-updated';
-    temporalDiv.innerHTML = this.innerContentHtml;
+    this.temporalDiv = document.createElement('div');
+    this.temporalDiv.id = 'temporal-updated';
+    this.temporalDiv.innerHTML = this.innerContentHtml;
     
-    viewerDiv.append(temporalDiv);
+    viewerDiv.append(this.temporalDiv);
     let rangeOne = document.querySelector('input[name="rangeOne"]'),
       rangeTwo = document.querySelector('input[name="rangeTwo"]'),
       outputOne = document.querySelector('.outputOne'),
@@ -68,9 +73,19 @@ export class LayerExtension {
 
     let geometryLayers = this.layerManager.getGeometryLayers();
     let layerManager = this.layerManager;
+    console.log(this.layerManager);
+
+
+    // debugger
+    // console.log(geometryLayers[9].object3d.getWorldPosition());
+    // debugger
+    
+    // geometryLayers[9].object3d.position.z += 20;
+    // geometryLayers[9].object3d.children[0].children[0].position.z += 20;
+    this.layerManager.notifyChange();
     rangeOne.oninput = function(){
       if (parseInt(this.value * 0.71) + 1950 > 2000)
-        geometryLayers[2].visible = false;
+        geometryLayers[9].visible = false;
       layerManager.notifyChange();
     };
 
@@ -83,5 +98,39 @@ export class LayerExtension {
         updateView.call(this);
       });
     });
+  }
+
+  createdDotElementData(){
+    
+    let berlietData = [this.layerManager.tilesManagers[8],
+      this.layerManager.tilesManagers[9],
+      this.layerManager.tilesManagers[10],
+      this.layerManager.tilesManagers[11],
+      this.layerManager.tilesManagers[12],
+      this.layerManager.tilesManagers[13]
+    ];
+
+    let allTime = [1954, 1966, 1978, 1986, 1993, 2021];
+
+
+    //Create html element
+    for (let i = 0 ; i < berlietData.length; i++){
+
+      let dotElement = document.createElement('span');
+      dotElement.className = 'dot';
+
+      //Transform decimal color in hexa
+      let c = new udviz.THREE.Color(); 
+      c.set(berlietData[i].color);
+      dotElement.style.backgroundColor = '#' + c.getHexString();
+
+      let time = allTime[i];
+      dotElement.style.left = (((time - 1950) * 600) / 71 - 2).toString() + 'px' ;
+      document.getElementsByClassName('range-slider container')[0].append(dotElement);
+    }
+    
+
+    
+    
   }
 }
--- src/bootstrap.js
+++ src/bootstrap.js
@@ -46,31 +46,30 @@ app.start('../assets/config/config.json').then((config) => {
   app.view3D.layerManager.registerStyle('grow', selectionStyle);
 
   const temporalExtension = new LayerExtension(app.view3D.layerManager);
-  temporalExtension.windowCreated();
+  // temporalExtension.windowCreated();
 
   // Declare the source for the data on Ariege area
-  const batBerlietSource = new udviz.itowns.FileSource({
-    url: 'https://raw.githubusercontent.com/VCityTeam/UD-Demo-TIGA-Morphogenese-Lyon/master/assets/Bat_Berliet_1993_l93_3946.geojson?token=GHSAT0AAAAAAB23Z4DAYGNKONIPGIB7AL6MY34VAHA',
-    crs: 'EPSG:3946',
-    format: 'application/json',
-  });
-  // Create a ColorLayer for the Ariege area
-  const berlierLayer = new udviz.itowns.ColorLayer('Berliet', {
-    name: 'berliet',
-    transparent: true,
-    source: batBerlietSource,
-    style: new udviz.itowns.Style({
-      fill: {
-        color: 'orange',
-        opacity: 0.9,
-      },
-      stroke: {
-        color: 'white',
-      },
-    }),
-  });
-  app.view3D.getItownsView().addLayer(berlierLayer);
-  // app.addModuleView('temporalExtension', temporelExtension);
+  // const batBerlietSource = new udviz.itowns.FileSource({
+  //   url: 'https://raw.githubusercontent.com/VCityTeam/UD-Demo-TIGA-Morphogenese-Lyon/master/assets/Bat_Berliet_1993_l93_3946.geojson?token=GHSAT0AAAAAAB23Z4DBGR6KX5NAYGOJZANOY3574RQ',
+  //   crs: 'EPSG:3946',
+  //   format: 'application/json',
+  // });
+  // // Create a ColorLayer for the Ariege area
+  // const berlierLayer = new udviz.itowns.ColorLayer('Berliet', {
+  //   name: 'berliet',
+  //   transparent: true,
+  //   source: batBerlietSource,
+  //   style: new udviz.itowns.Style({
+  //     fill: {
+  //       color: 'orange',
+  //       opacity: 0.9,
+  //     },
+  //     stroke: {
+  //       color: 'white',
+  //     },
+  //   })
+  // });
+  // app.view3D.getItownsView().addLayer(berlierLayer);
 
   ////// TEMPORAL MODULE
   // const temporalModule = new udviz.Widgets.TemporalModule(
