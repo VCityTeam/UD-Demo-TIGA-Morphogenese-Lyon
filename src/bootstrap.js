116e743dc0ab467b39041ae947cff1aae83c8af9
CorentinLIRIS <corentin.chignin@hotmail.fr>
2023-02-14 12:01:42 +0100

    Test with multiple layers

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,6 +1,8 @@
 /** @format */
 import * as udviz from 'ud-viz';
 import {LayerExtension} from './TemporelExtension/LayerExtension.js';
+import { $3DTemporalExtension } from 'ud-viz/src/Widgets/Temporal/Model/3DTemporalExtension';
+import { TemporalProvider } from 'ud-viz/src/Widgets/Temporal/ViewModel/TemporalProvider';
 
 const app = new udviz.Templates.AllWidget();
 
@@ -73,10 +75,33 @@ app.start('../assets/config/config.json').then((config) => {
   // app.view3D.getItownsView().addLayer(berlierLayer);
 
   //// TEMPORAL MODULE
-  const temporalModule = new udviz.Widgets.TemporalModule(
-    app.view3D.getLayerManager().tilesManagers[0],
-    app.config.temporalModule
-  );
-  app.addModuleView('temporal', temporalModule.view);
+  // const temporalModule = new udviz.Widgets.TemporalModule(
+  //   app.view3D.getLayerManager().tilesManagers[0],
+  //   app.config.temporalModule
+  // );
+  // app.addModuleView('temporal', temporalModule.view);
+  const tilesManagers = app.view3D.getLayerManager().tilesManagers;
+  for( let i = 0; i < tilesManagers.length; i++) {
+    
+    let model = new $3DTemporalExtension();
+
+    new TemporalProvider(
+      model,
+      tilesManagers[i],
+      2009 + i
+    );
+  }
+  
+
+  // // 2010
+  // model = new $3DTemporalExtension();
+
+  // new TemporalProvider(
+  //   model,
+  //   app.view3D.getLayerManager().tilesManagers[1],
+  //   2010
+  // );
+
+  // this.view = new TemporalView(this.provider, temporalOptions);
 
 });
